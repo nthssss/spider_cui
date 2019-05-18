e4d64abc95ffe6a8b3c3e6f821b68414d72140b9
nthssss <nocturn37e@outlook.com>
2019-05-18 18:41:27 +0800

    chapter3215：响应

---
+++ chapter32/chapter321/chapter3215.py
@@ -0,0 +1,12 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+r = requests.get('https://www.jianshu.com',headers=headers)
+exit() if not r.status_code == requests.codes.ok else print('Request Successfully')
+print(type(r.status_code), r.status_code)
+print(type(r.headers), r.headers)
+print(type(r.cookies), r.cookies)
+print(type(r.url), r.url)
+print(type(r.history), r.history)
