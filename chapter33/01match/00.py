9f8ff6bace246eb33544d0931c6afa1033c33277
nthssss <nocturn37e@outlook.com>
2019-05-18 19:40:30 +0800

    chapter33：01match

---
+++ chapter33/01match/00.py
@@ -0,0 +1,14 @@
+import re
+
+content = 'Hello 123 4567 World_This is a Regex Demo'
+print(len(content))
+result = re.match('^Hello\s\d\d\d\s\d{4}\s\w{10}', content)
+print(result)
+print(result.group())
+print(result.span())
+
+content = 'http://weibo.com/comment/kEraCN'
+result1 = re.match('http.*?comment/(.*?)', content)
+result2 = re.match('http.*?comment/(.*)', content)
+print('result1', result1.group(1))
+print('result2', result2.group(1))
